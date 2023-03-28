9dbf6eacd89335dfc8d917f074e75b3e9fc92387
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-03-28 10:23:44 +0530

    Added: Use RegEx For Name and Throw Error

---
+++ EmployeePayRollData.js
@@ -0,0 +1,55 @@
+//UC 13-14 Regex pattern
+class EmployeePayrollData 
+{
+    id;
+    salary;
+    gender;
+    startDate;
+
+    //Constructor
+    constructor(...params) 
+    {
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
+    }
+
+    // getter and setter
+    get name()
+    {
+        return this._name;
+    }
+
+    set name(name) 
+    {
+        let nameRegex = RegExp('[A-Z]{1}[a-z]{2,}');
+        if (nameRegex.test(name))
+            this._name = name;
+        else throw "Name is Incorrect";
+    }
+
+    // method
+    toString() 
+    {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = !this.startDate ? "undefined" : 
+        this.startDate.toLocaleDateString("en-In", options);
+        return "id = " + this.id + " Name = " + this.name + " Salary = " + this.salary + " Gender = " + this.gender + " Start Date : " + empDate;
+    }
+}
+
+
+let employeePayrollData = new EmployeePayrollData(1, "Mark", 4000);
+console.log(employeePayrollData.toString());
+try 
+{
+    employeePayrollData.name = "john";
+    console.log(employeePayrollData.toString());
+} catch (e) 
+{
+    console.error(e);
+}
+let newEmployeePayrollData = new EmployeePayrollData(1, "Terrisa", 4000, "F", new Date());
+console.log(newEmployeePayrollData.toString());
